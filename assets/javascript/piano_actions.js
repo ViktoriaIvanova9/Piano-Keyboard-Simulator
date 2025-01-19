c32bd4a0e79e15b15ce21579c18ebb2381ebc2c2
Nikolay Georgiev <nikoalisso1253@gmail.com>
2025-01-19 18:01:13 +0200

    added key value pair {buttons : sound}

--- assets/javascript/piano_actions.js
+++ assets/javascript/piano_actions.js
@@ -1,11 +1,44 @@
 const keys = document.querySelectorAll('.piano-keys');
+const pianoSound = new Audio();
 
-const pianoSound = new Audio('./assets/music_sounds/c4.mp3');
+// Map the keyboard keys to sound file names (you can adjust the mappings as needed)
+const keyMapping = {
+    'a': 'c4',
+    's': 'c_sharp',
+    'd': 'd',
+    'f': 'd#',
+    'g': 'e',
+    'h': 'f',
+    'j': 'f#',
+    'k': 'g',
+    'l': 'g#',
+    ';': 'a',
+    '4': 'a#',
+    '5': 'b',
+    '6': 'c5',
+};
 
+// Function to play the sound for a given key
+function playSound(key) {
+
+    if (keyMapping[key]) {
+        pianoSound.src = `./assets/music_sounds/${keyMapping[key]}.mp3`;
+        pianoSound.play();
+    }
+}
+
+// Add event listener for mouse clicks on piano keys
 keys.forEach((key) => {
     key.addEventListener('click', (e) => {
-        const clickedKey = e.target.dataset.list;
-        pianoSound.src = './assets/music_sounds/c4.mp3';
+        const clickedKey = e.target.dataset.note;
+        console.log(clickedKey)
+        pianoSound.src = `./assets/music_sounds/${clickedKey}.mp3`;
         pianoSound.play();
     });
+});
+
+document.addEventListener('keydown', (e) => {
+    const keyPressed = e.key.toLowerCase();  // Get the key that was pressed, convert to lowercase
+    console.log(`Key pressed: ${keyPressed}`); // Debugging log for keyboard input
+    playSound(keyPressed);  // Call the playSound function to play the corresponding sound
 });
--- index.php
+++ index.php
@@ -9,48 +9,50 @@
     <link rel="stylesheet" type="text/css" href="./assets/css/piano_styles.css?v=1.0">
     <script src="./assets/javascript/piano_actions.js?v=1.0" defer></script>
 </head>
+
 <body>
     <div class="container">
         <div class="piano-container">
             <ul class="piano-keys-list">
-                <li class="piano-keys white-key" data-key="01"></li>
-                <li class="piano-keys black-key" data-key="02"></li>
-                <li class="piano-keys white-key" data-key="03"></li>
-                <li class="piano-keys black-key" data-key="04"></li>
-                <li class="piano-keys white-key" data-key="05"></li>
-                <li class="piano-keys white-key" data-key="06"></li>
-                <li class="piano-keys black-key" data-key="07"></li>
-                <li class="piano-keys white-key" data-key="08"></li>
-                <li class="piano-keys black-key" data-key="09"></li>
-                <li class="piano-keys white-key" data-key="10"></li>
-                <li class="piano-keys black-key" data-key="11"></li>
-                <li class="piano-keys white-key" data-key="12"></li>
-                <li class="piano-keys white-key" data-key="13"></li>
-                <li class="piano-keys black-key" data-key="14"></li>
-                <li class="piano-keys white-key" data-key="15"></li>
-                <li class="piano-keys black-key" data-key="16"></li>
-                <li class="piano-keys white-key" data-key="17"></li>
-                <li class="piano-keys white-key" data-key="18"></li>
-                <li class="piano-keys black-key" data-key="19"></li>
-                <li class="piano-keys white-key" data-key="20"></li>
-                <li class="piano-keys black-key" data-key="21"></li>
-                <li class="piano-keys white-key" data-key="22"></li>
-                <li class="piano-keys black-key" data-key="23"></li>
-                <li class="piano-keys white-key" data-key="24"></li>
-                <li class="piano-keys white-key" data-key="25"></li>
-                <li class="piano-keys black-key" data-key="26"></li>
-                <li class="piano-keys white-key" data-key="27"></li>
-                <li class="piano-keys black-key" data-key="28"></li>
-                <li class="piano-keys white-key" data-key="29"></li>
-                <li class="piano-keys white-key" data-key="30"></li>
-                <li class="piano-keys black-key" data-key="31"></li>
-                <li class="piano-keys white-key" data-key="32"></li>
-                <li class="piano-keys black-key" data-key="33"></li>
-                <li class="piano-keys white-key" data-key="34"></li>
-                <li class="piano-keys black-key" data-key="35"></li>
-                <li class="piano-keys white-key" data-key="36"></li>
+                <li class="piano-keys white-key" data-key="01" data-note="c4"></li>
+                <li class="piano-keys black-key" data-key="02" data-note="c#"></li>
+                <li class="piano-keys white-key" data-key="03" data-note="d"></li>
+                <li class="piano-keys black-key" data-key="04" data-note="d#"></li>
+                <li class="piano-keys white-key" data-key="05" data-note="e"></li>
+                <li class="piano-keys white-key" data-key="06" data-note="f"></li>
+                <li class="piano-keys black-key" data-key="07" data-note="f#"></li>
+                <li class="piano-keys white-key" data-key="08" data-note="g"></li>
+                <li class="piano-keys black-key" data-key="09" data-note="g#"></li>
+                <li class="piano-keys white-key" data-key="10" data-note="a"></li>
+                <li class="piano-keys black-key" data-key="11" data-note="a#"></li>
+                <li class="piano-keys white-key" data-key="12" data-note="b"></li>
+                <li class="piano-keys white-key" data-key="13" data-note="c5"></li>
+                <li class="piano-keys black-key" data-key="14" data-note="c#5"></li>
+                <li class="piano-keys white-key" data-key="15" data-note="d5"></li>
+                <li class="piano-keys black-key" data-key="16" data-note="d#5"></li>
+                <li class="piano-keys white-key" data-key="17" data-note="e5"></li>
+                <li class="piano-keys white-key" data-key="18" data-note="f5"></li>
+                <li class="piano-keys black-key" data-key="19" data-note="f#5"></li>
+                <li class="piano-keys white-key" data-key="20" data-note="g5"></li>
+                <li class="piano-keys black-key" data-key="21" data-note="g#5"></li>
+                <li class="piano-keys white-key" data-key="22" data-note="a5"></li>
+                <li class="piano-keys black-key" data-key="23" data-note="a#5"></li>
+                <li class="piano-keys white-key" data-key="24" data-note="b5"></li>
+                <li class="piano-keys white-key" data-key="25" data-note="c6"></li>
+                <li class="piano-keys black-key" data-key="26" data-note="c#6"></li>
+                <li class="piano-keys white-key" data-key="27" data-note="d6"></li>
+                <li class="piano-keys black-key" data-key="28" data-note="d#6"></li>
+                <li class="piano-keys white-key" data-key="29" data-note="e6"></li>
+                <li class="piano-keys white-key" data-key="30" data-note="f6"></li>
+                <li class="piano-keys black-key" data-key="31" data-note="f#6"></li>
+                <li class="piano-keys white-key" data-key="32" data-note="g6"></li>
+                <li class="piano-keys black-key" data-key="33" data-note="g#6"></li>
+                <li class="piano-keys white-key" data-key="34" data-note="a6"></li>
+                <li class="piano-keys black-key" data-key="35" data-note="a#6"></li>
+                <li class="piano-keys white-key" data-key="36" data-note="b6"></li>
             </ul>
         </div>
     </div>
 </body>
+
 </html>
